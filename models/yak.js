eda5301f693d00ee01f5b3d223932a56dd12280f
korycfitz <korycfitz@gmail.com>
2023-04-24 21:01:52 -0400

    fix bug with dockerfile so deployment link works

--- models/yak.js
+++ models/yak.js
@@ -2,6 +2,12 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  content: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  likes: [{type: Schema.Types.ObjectId, ref: 'Like' }],
+})
+
 const yakSchema = new Schema({
   message: { type: String, required: true },
   likes: [{type: Schema.Types.ObjectId, ref: 'Like' }],
