42d463c1c5463cf4ad23e8875e0fd52d627f6508
korycfitz <korycfitz@gmail.com>
2023-04-26 12:29:08 -0400

    add functionality to redirect to all yaks page instead of index view

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,14 @@ import passport from 'passport'
 const router = Router()
 
 router.post("/google", passport.authenticate("google-one-tap", {
-  failureRedirect: "/",
+  failureRedirect: "/yaks",
   successRedirect: "/yaks",
 }))
 
 router.get('/logout', function (req, res, next) {
   req.logout(function(err) {
     if (err) { return next(err)}
-    res.redirect('/')
+    res.redirect('/yaks')
   })
 })
 
